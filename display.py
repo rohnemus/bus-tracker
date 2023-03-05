3d7054d9984a5c66d329bc0f3cac89ac4a9a6960
rohnemus <russell.ohnemus@gmail.com>
2023-03-05 01:55:29 -0800

    Remove print statements from display.py

--- display.py
+++ display.py
@@ -18,17 +18,7 @@ class DisplayBus(SampleBase):
         pos = offscreen_canvas.width
 
         while True:
-            # offscreen_canvas.Clear()
-            # len = graphics.DrawText(offscreen_canvas, font, pos, 10, textColor, my_text)
-            # pos -= 1
-            # if (pos + len < 0):
-            #     pos = offscreen_canvas.width
-
-            # time.sleep(0.05)
-            # offscreen_canvas = self.matrix.SwapOnVSync(offscreen_canvas)
-
             offscreen_canvas.Clear()
-            print("\nRefreshing Data!")
             graphics.DrawText(offscreen_canvas, font, 2, 14, textColor, "Refreshing")
             graphics.DrawText(offscreen_canvas, font, 2, 28, textColor, "   Data   ")
             offscreen_canvas = self.matrix.SwapOnVSync(offscreen_canvas)
@@ -37,16 +27,12 @@ class DisplayBus(SampleBase):
                 visits = bstk.get_predictions([14159,14158],['IB'])
 
                 for i in range(60):
-                    print("")
-                    print(i)
                     incomming_busses = bstk.update_predictions(visits,i)
                     parsed = bstk.parse_predictions(incomming_busses)
                     for predictions in parsed:
-                        print(predictions)
 
                     if len(parsed) < 1:
                         offscreen_canvas.Clear()
-                        print("\nNo Buses Predicted")
                         graphics.DrawText(offscreen_canvas, font, 8, 14, textColor, "No Buses")
                         graphics.DrawText(offscreen_canvas, font, 5, 28, textColor, "Predicted")                    
 
@@ -64,7 +50,6 @@ class DisplayBus(SampleBase):
 
             except:
                 offscreen_canvas.Clear()
-                print("\nNetwork Error\n")
                 graphics.DrawText(offscreen_canvas, font, 5, 14, textColor, " Network ")
                 graphics.DrawText(offscreen_canvas, font, 5, 28, textColor, "  Error  ")
                 offscreen_canvas = self.matrix.SwapOnVSync(offscreen_canvas)
