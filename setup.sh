f3871eaa9a9a752e1b763b84635e843671be08f1
rohnemus <russell.ohnemus@gmail.com>
2023-03-05 00:05:03 -0800

    Add status updates to setup script

--- setup.sh
+++ setup.sh
@@ -2,23 +2,27 @@
 
 
 # Set script to run on startup
+echo "Adding bus-tracker.sh to run on startup"
 echo "" >> ~/.bashrc
 echo "# Run bus tracker display script on boot:" >> ~/.bashrc
 echo "~/bus-tracker/bus-tracker/bus-tracker.sh &" >> ~/.bashrc
 source ~/.bashrc
 
 # Initialize git submodule
+echo "Initializing git submodule"
 cd ~/bus-tracker/bus-tracker
 git submodule init
 git submodule update --init --recursive
 
 # Install Python Bindings
+echo "Building Python Bindings"
 cd ~/bus-tracker/bus-tracker/matrix
 sudo apt-get update && sudo apt-get install python3-dev python3-pillow -y
 make build-python PYTHON=$(command -v python3)
 sudo make install-python PYTHON=$(command -v python3)
 
-# Move python script to sample directory
+# Move python scripts to run directory
+echo "Moving scripts to matrix/bindings/python/samples"
 cd ~/bus-tracker/bus-tracker
 cp bustracker.py display.py matrix/bindings/python/samples
 
