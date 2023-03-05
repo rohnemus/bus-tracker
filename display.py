6c8e97b4ba9460b14d3152cba0b0fe9def8b725b
rohnemus <russell.ohnemus@gmail.com>
2023-03-04 19:58:42 -0800

    First pass at display script

---
+++ display.py
@@ -0,0 +1,64 @@
+#!/usr/bin/env python
+# Display bus times on screen.
+from samplebase import SampleBase
+from rgbmatrix import graphics
+import bustracker as bstk
+import time
+
+
+class DisplayBus(SampleBase):
+    def __init__(self, *args, **kwargs):
+        super(DisplayBus, self).__init__(*args, **kwargs)
+
+    def run(self):
+        offscreen_canvas = self.matrix.CreateFrameCanvas()
+        font = graphics.Font()
+        font.LoadFont("../../../fonts/7x13.bdf")
+        textColor = graphics.Color(255, 0, 0)
+        pos = offscreen_canvas.width
+        my_text = self.args.text
+
+        while True:
+            offscreen_canvas.Clear()
+            len = graphics.DrawText(offscreen_canvas, font, pos, 10, textColor, my_text)
+            pos -= 1
+            if (pos + len < 0):
+                pos = offscreen_canvas.width
+
+            time.sleep(0.05)
+            offscreen_canvas = self.matrix.SwapOnVSync(offscreen_canvas)
+
+            offscreen_canvas.Clear()
+            print("\nRefreshing Data!")
+            graphics.DrawText(offscreen_canvas, font, 10, 15, textColor, "Refreshing")
+            graphics.DrawText(offscreen_canvas, font, 10, 29, textColor, "   Data   ")
+
+            try:
+                visits = bstk.get_predictions([14159,14158],['IB'])
+
+                for i in range(60):
+                    print("")
+                    print(i)
+                    incomming_busses = bstk.update_predictions(visits,i)
+                    parsed = bstk.parse_predictions(incomming_busses)
+                    for predictions in parsed:
+                        print(predictions)
+
+                    graphics.DrawText(offscreen_canvas, font, 10, 15, textColor, str(parsed[0]))
+                    graphics.DrawText(offscreen_canvas, font, 10, 29, textColor, str(parsed[1]))
+
+                    time.sleep(1)
+
+            except:
+                offscreen_canvas.Clear()
+                print("\nNetwork Error\n")
+                graphics.DrawText(offscreen_canvas, font, 10, 15, textColor, " Network ")
+                graphics.DrawText(offscreen_canvas, font, 10, 29, textColor, "  Error  ")
+                time.sleep(5)
+
+
+# Main function
+if __name__ == "__main__":
+    run_text = DisplayBus()
+    if (not run_text.process()):
+        run_text.print_help()
