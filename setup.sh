93cba7e31d43fa016d0c743275553a8489736240
rohnemus <russell.ohnemus@gmail.com>
2023-03-05 02:36:12 -0800

    Update setup.sh with systemd

--- setup.sh
+++ setup.sh
@@ -2,19 +2,30 @@
 
 
 # Set script to run on startup
-echo "Adding bus-tracker.sh to run on startup"
-echo "" >> ~/.bashrc
-echo "# Run bus tracker display script on boot:" >> ~/.bashrc
-echo "~/bus-tracker/bus-tracker/bus-tracker.sh &" >> ~/.bashrc
-source ~/.bashrc
+echo ""
+echo "Creating service to run bus-tracker.sh on startup"
+sudo cat > ~/etc/systemd/system/bus_tracker.service << EOF
+[Unit]
+Description=Display bus times
+After=network.target
+
+[Service]
+ExecStart=~/bus-tracker/bus-tracker/bus-tracker.sh
+Restart=on-failure
+
+[Install]
+WantedBy=multi-user.target
+EOF
 
 # Initialize git submodule
+echo ""
 echo "Initializing git submodule"
 cd ~/bus-tracker/bus-tracker
 git submodule init
 git submodule update --init --recursive
 
 # Install Python Bindings
+echo ""
 echo "Building Python Bindings"
 cd ~/bus-tracker/bus-tracker/matrix
 sudo apt-get update && sudo apt-get install python3-dev python3-pillow -y
@@ -22,7 +33,15 @@ make build-python PYTHON=$(command -v python3)
 sudo make install-python PYTHON=$(command -v python3)
 
 # Move python scripts to run directory
+echo ""
 echo "Moving scripts to matrix/bindings/python/samples"
 cd ~/bus-tracker/bus-tracker
 cp bustracker.py display.py matrix/bindings/python/samples
+
+# Enable systemd service and start it
+echo ""
+echo "Enabling systemd"
+sudo systemctl daemon-reload
+sudo systemctl enable shellscript.service 
+sudo systemctl start shellscript.service 
 
