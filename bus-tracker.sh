ccb6b0daa48c120f4d8de2889f54f9f7e19f7129
rohnemus <russell.ohnemus@gmail.com>
2023-03-04 22:44:01 -0800

    Update setup script for full install

--- bus-tracker.sh
+++ bus-tracker.sh
@@ -3,6 +3,4 @@
 echo "Starting bus tracker"
 
 cd matrix/bindings/python/samples
-sudo ./runtext.py --led-gpio-mapping=adafruit-hat --led-cols=64 --led-rows=32 &
-
-echo "Script Done"
+sudo python display.py --led-gpio-mapping=adafruit-hat --led-cols=64 --led-rows=32 
