9ff6026cfa552fe7ca508dcb68e12ed12ca37300
rohnemus <russell.ohnemus@gmail.com>
2023-03-05 04:34:23 -0800

    Change setup.sh to actually initalize systemd

--- setup.sh
+++ setup.sh
@@ -4,18 +4,22 @@
 # Set script to run on startup
 echo ""
 echo "Creating service to run bus-tracker.sh on startup"
-sudo cat > ~/etc/systemd/system/bus_tracker.service << EOF
+cat > bus_tracker.service << EOF
 [Unit]
 Description=Display bus times
 After=network.target
 
 [Service]
-ExecStart=~/bus-tracker/bus-tracker/bus-tracker.sh
+ExecStart=/bin/bash ${HOME}/bus-tracker/bus-tracker/bus-tracker.sh
 Restart=on-failure
+Type=forking
+User=${USER}
+WorkingDirectory=${HOME}/bus-tracker/bus-tracker
 
 [Install]
 WantedBy=multi-user.target
 EOF
+sudo mv bus_tracker.service /etc/systemd/system
 
 # Initialize git submodule
 echo ""
@@ -37,11 +41,12 @@ echo ""
 echo "Moving scripts to matrix/bindings/python/samples"
 cd ~/bus-tracker/bus-tracker
 cp bustracker.py display.py matrix/bindings/python/samples
+cp bus-tracker.sh /usr/local/bin
 
 # Enable systemd service and start it
 echo ""
 echo "Enabling systemd"
 sudo systemctl daemon-reload
-sudo systemctl enable shellscript.service 
-sudo systemctl start shellscript.service 
-
+sudo systemctl enable bus_tracker.service 
+sudo systemctl start bus_tracker.service 
+sudo systemctl status bus_tracker.service
